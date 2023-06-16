c7cb1b3a074b2536a20d4be707c0d2cb62bdf03f
sueqixue <xueqi923145227@gmail.com>
2023-06-16 17:16:10 -0400

    Add fly_src_to_dest.py

---
+++ gym_pybullet_drones/examples/fly_src_to_dest.py
@@ -0,0 +1,212 @@
+"""Script demonstrating the ground effect contribution.
+
+The simulation is run by a `CtrlAviary` environment.
+
+Example
+-------
+In a terminal, run as:
+
+    $ python hover.py
+
+Notes
+-----
+Let the drone take off and fly from a src position to a dest position.
+Use to test different controllers.
+
+"""
+import os
+import time
+import argparse
+from datetime import datetime
+import pdb
+import math
+import random
+import numpy as np
+import pybullet as p
+import matplotlib.pyplot as plt
+
+from gym_pybullet_drones.utils.enums import DroneModel, Physics
+from gym_pybullet_drones.envs.CtrlAviary import CtrlAviary
+from gym_pybullet_drones.envs.VisionAviary import VisionAviary
+from gym_pybullet_drones.control.DSLPIDControl import DSLPIDControl
+from gym_pybullet_drones.control.SimplePIDControl import SimplePIDControl
+from gym_pybullet_drones.utils.Logger import Logger
+from gym_pybullet_drones.utils.utils import sync, str2bool
+
+
+
+DEFAULT_DRONES = DroneModel("cf2x")
+DEFAULT_NUM_DRONES = 1
+DEFAULT_PHYSICS = Physics.PYB_GND ## or Physics.PYB for comparision
+DEFAULT_GUI = True
+DEFAULT_RECORD_VIDEO = False
+DEFAULT_PLOT = True
+DEFAULT_USER_DEBUG_GUI = False
+DEFAULT_AGGREGATE = True
+DEFAULT_OBSTACLES = False
+DEFAULT_SIMULATION_FREQ_HZ = 240
+DEFAULT_CONTROL_FREQ_HZ = 240
+DEFAULT_DURATION_SEC = 8
+DEFAULT_OUTPUT_FOLDER = 'results'
+DEFAULT_COLAB = False
+DEFAULT_GD = False
+
+def run(
+        drone=DEFAULT_DRONES,
+        num_drones=DEFAULT_NUM_DRONES,
+        physics=DEFAULT_PHYSICS,
+        gui=DEFAULT_GUI,
+        record_video=DEFAULT_RECORD_VIDEO,
+        plot=DEFAULT_PLOT,
+        user_debug_gui=DEFAULT_USER_DEBUG_GUI,
+        aggregate=DEFAULT_AGGREGATE,
+        obstacles=DEFAULT_OBSTACLES,
+        simulation_freq_hz=DEFAULT_SIMULATION_FREQ_HZ,
+        control_freq_hz=DEFAULT_CONTROL_FREQ_HZ,
+        duration_sec=DEFAULT_DURATION_SEC,
+        output_folder=DEFAULT_OUTPUT_FOLDER,
+        colab=DEFAULT_COLAB,
+        ground_effect=DEFAULT_GD
+        ):
+
+    #### Initialize the simulation #############################
+    INIT_XYZ = np.array([0, 0, 0]).reshape(1,3)
+    HOVER_XYZ = np.array([0, 0, 1]).reshape(1,3)
+    DEST_XYZ = np.array([1, 1, 1]).reshape(1,3)
+    AGGR_PHY_STEPS = int(simulation_freq_hz/control_freq_hz) if aggregate else 1
+
+    #### Initialize a vertical trajectory ######################
+    PERIOD = 8
+    HOVER_PAR = 0.15*3
+    HOVER_H = 1
+    NUM_WP = control_freq_hz*PERIOD
+    TARGET_POS = np.zeros((NUM_WP,3))
+    HOVER_FLAG = False
+    GD_HOVER = ground_effect
+
+    if GD_HOVER:
+        print(f"---------- TAKE OFF WITH GROUND EFFECT ----------\n")
+    else:
+        print(f"---------- TAKE OFF WITHOUT GROUND EFFECT ----------\n")
+
+    for i in range(NUM_WP):
+    """
+    TODO: taking off the drone to the hover xyz and move to the dest position.
+        Note that TARGET_POS is an array contains the position at each sampling
+        point, which means it contains the infomation of the desired trajectory/
+        path of the current task. So when implementing any collision avoidance
+        algorithms/controller, use the input src and dest position to find the
+        optimal trajectory/path.
+    """
+        # TARGET_POS[i, :] = INIT_XYZ[0, 0], INIT_XYZ[0, 1], INIT_XYZ[0, 2] + 0.15 * (np.sin((i/NUM_WP)*(2*np.pi)) + 1)
+        if GD_HOVER:
+            if not HOVER_FLAG:
+                TARGET_POS[i, :] = INIT_XYZ[0, 0], INIT_XYZ[0, 1], INIT_XYZ[0, 2] + HOVER_PAR * (np.sin((i/NUM_WP)*(2*np.pi)) + 1)
+            else:
+                TARGET_POS[i, :] = INIT_XYZ[0, 0], INIT_XYZ[0, 1], TARGET_POS[i-1, 2]
+
+            if TARGET_POS[i, 2] < TARGET_POS[i-1, 2]:
+                HOVER_FLAG = True
+        else:
+            TARGET_POS[i, :] = INIT_XYZ[0, 0], INIT_XYZ[0, 1], INIT_XYZ[0, 2] + i * (HOVER_H/NUM_WP)
+
+        # print(f"TARGET_POS[{i}, :] = {TARGET_POS[i, :]}")
+    wp_counter = 0
+
+    #### Create the environment ################################
+    env = CtrlAviary(drone_model=drone,
+                     num_drones=num_drones,
+                     initial_xyzs=INIT_XYZ,
+                     physics=physics,
+                     neighbourhood_radius=10,
+                     freq=simulation_freq_hz,
+                     aggregate_phy_steps=AGGR_PHY_STEPS,
+                     gui=gui,
+                     record=record_video,
+                     obstacles=obstacles,
+                     user_debug_gui=user_debug_gui
+                     )
+
+    #### Obtain the PyBullet Client ID from the environment ####
+    PYB_CLIENT = env.getPyBulletClient()
+
+    #### Initialize the logger #################################
+    logger = Logger(logging_freq_hz=int(simulation_freq_hz/AGGR_PHY_STEPS),
+                    num_drones=num_drones,
+                    output_folder=output_folder,
+                    colab=colab
+                    )
+
+    #### Initialize the controller #############################
+    ctrl = DSLPIDControl(drone_model=drone)
+
+    #### Run the simulation ####################################
+    CTRL_EVERY_N_STEPS = int(np.floor(env.SIM_FREQ/control_freq_hz))
+    action = {"0": np.array([0,0,0,0])}
+    START = time.time()
+    for i in range(0, int(duration_sec*env.SIM_FREQ), AGGR_PHY_STEPS):
+
+        #### Step the simulation ###################################
+        obs, reward, done, info = env.step(action)
+
+        #### Compute control at the desired frequency ##############
+        if i%CTRL_EVERY_N_STEPS == 0:
+
+            #### Compute control for the current way point #############
+            action["0"], _, _ = ctrl.computeControlFromState(control_timestep=CTRL_EVERY_N_STEPS*env.TIMESTEP,
+                                                             state=obs["0"]["state"],
+                                                             target_pos=TARGET_POS[wp_counter, :],
+                                                             )
+
+            #### Go to the next way point and loop #####################
+            wp_counter = wp_counter + 1 if wp_counter < (NUM_WP-1) else 0
+
+        #### Log the simulation ####################################
+        logger.log(drone=0,
+                   timestamp=i/env.SIM_FREQ,
+                   state= obs["0"]["state"],
+                   control=np.hstack([TARGET_POS[wp_counter, :], np.zeros(9)])
+                   )
+
+        #### Printout ##############################################
+        if i%env.SIM_FREQ == 0:
+            env.render()
+
+        #### Sync the simulation ###################################
+        if gui:
+            sync(i, START, env.TIMESTEP)
+
+    #### Close the environment #################################
+    env.close()
+
+    #### Save the simulation results ###########################
+    logger.save()
+    logger.save_as_csv("gnd") # Optional CSV save
+
+    #### Plot the simulation results ###########################
+    if plot:
+        logger.plot()
+
+if __name__ == "__main__":
+    #### Define and parse (optional) arguments for the script ##
+    parser = argparse.ArgumentParser(description='Hover script with or without ground effect using CtrlAviary and DSLPIDControl')
+    parser.add_argument('--drone',              default=DEFAULT_DRONES,             type=DroneModel,    help='Drone model (default: CF2X)', metavar='', choices=DroneModel)
+    parser.add_argument('--num_drones',         default=DEFAULT_NUM_DRONES,         type=int,           help='Number of drones (default: 3)', metavar='')
+    parser.add_argument('--physics',            default=DEFAULT_PHYSICS,            type=Physics,       help='Physics updates (default: PYB)', metavar='', choices=Physics)
+    parser.add_argument('--gui',                default=DEFAULT_GUI,                type=str2bool,      help='Whether to use PyBullet GUI (default: True)', metavar='')
+    parser.add_argument('--record_video',       default=DEFAULT_RECORD_VIDEO,       type=str2bool,      help='Whether to record a video (default: False)', metavar='')
+    parser.add_argument('--plot',               default=DEFAULT_PLOT,               type=str2bool,      help='Whether to plot the simulation results (default: True)', metavar='')
+    parser.add_argument('--user_debug_gui',     default=DEFAULT_USER_DEBUG_GUI,     type=str2bool,      help='Whether to add debug lines and parameters to the GUI (default: False)', metavar='')
+    parser.add_argument('--aggregate',          default=DEFAULT_AGGREGATE,          type=str2bool,      help='Whether to aggregate physics steps (default: False)', metavar='')
+    parser.add_argument('--obstacles',          default=DEFAULT_OBSTACLES,          type=str2bool,      help='Whether to add obstacles to the environment (default: True)', metavar='')
+    parser.add_argument('--simulation_freq_hz', default=DEFAULT_SIMULATION_FREQ_HZ, type=int,           help='Simulation frequency in Hz (default: 240)', metavar='')
+    parser.add_argument('--control_freq_hz',    default=DEFAULT_CONTROL_FREQ_HZ,    type=int,           help='Control frequency in Hz (default: 48)', metavar='')
+    parser.add_argument('--duration_sec',       default=DEFAULT_DURATION_SEC,       type=int,           help='Duration of the simulation in seconds (default: 5)', metavar='')
+    parser.add_argument('--output_folder',      default=DEFAULT_OUTPUT_FOLDER,      type=str,           help='Folder where to save logs (default: "results")', metavar='')
+    parser.add_argument('--colab',              default=DEFAULT_COLAB,              type=bool,          help='Whether example is being run by a notebook (default: "False")', metavar='')
+    parser.add_argument('--ground_effect',      dest='ground_effect',       action='store_true')
+    parser.add_argument('--no_ground_effect',   dest='ground_effect',       action='store_false')
+    parser.set_defaults(ground_effect=False)
+    ARGS = parser.parse_args()
+
+    run(**vars(ARGS))
