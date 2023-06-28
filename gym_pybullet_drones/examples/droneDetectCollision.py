4f143e33ad251a2c97c9e6d726dffafeebf8ffb4
sueqixue <xueqi923145227@gmail.com>
2023-06-28 16:59:07 -0400

    Update collision detection

--- gym_pybullet_drones/examples/droneDetectCollision.py
+++ gym_pybullet_drones/examples/droneDetectCollision.py
@@ -1,6 +1,9 @@
 import numpy as np
 import pybullet as p
 
+# Debug boolens
+PRINTING = False
+
 def droneDetectCollision(physics_client_id, drone_id, obstacles_id_list, drone_pos):
     """
     Check if the drone position is collided with any obstacle in the
@@ -9,15 +12,19 @@ def droneDetectCollision(physics_client_id, drone_id, obstacles_id_list, drone_p
     :param drone_id: int which is the pybullet id of the drone
     :param obstacle_id_list: array of int which contains the pybullet id of all obstacles
     :param drone_pos: the future/potential positin of the drone
-    "return: boolean, true if the drone is in collision with any obstacle
+    :return: boolean, true if the drone is in collision with any obstacle
     """
-    print(f"Start collision detection with physics_client_id = {physics_client_id}\n")
-    print(f"drone_id = {drone_id[0]}\n")
+    if PRINTING:
+        print(f"Start collision detection with physics_client_id = {physics_client_id}\n")
+        print(f"drone_id = {drone_id[0]}\n")
+
     p.performCollisionDetection(physics_client_id)
 
     n_obstacles = len(obstacles_id_list)
     for i in range(n_obstacles):
-        print(f"Checking obstacle {i}, id = {obstacles_id_list[i]}\n")
+        if PRINTING:
+            print(f"Checking obstacle {i}, id = {obstacles_id_list[i]}\n")
+
         if detectCollisionEachObstacle(physics_client_id, drone_id[0], obstacles_id_list[i], drone_pos):
             return True
     return False
@@ -30,9 +37,11 @@ def detectCollisionEachObstacle(physics_client_id, drone_id, obstacle_id, drone_
     :param drone_id: int which is the pybullet id of the drone
     :param obstacle_id: int which is the pybullet id of the obstacle
     :param drone_pos: the future/potential positin of the drone
-    "return: boolean, true if the drone is in collision with this obstacle
+    :return: boolean, true if the drone is in collision with this obstacle
     """
-    print(f"Calling getContactPoints...\n")
+    if PRINTING:
+        print(f"Calling getContactPoints...\n")
+
     contact_points_list = p.getContactPoints(bodyA=drone_id,
                    bodyB=obstacle_id,
                    physicsClientId=physics_client_id
