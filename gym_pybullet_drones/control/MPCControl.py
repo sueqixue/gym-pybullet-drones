e29d23f429a2e3b70145a29a8b562813b3babcfa
sueqixue <xueqi923145227@gmail.com>
2023-11-02 16:18:24 -0400

    Update MPC control

--- gym_pybullet_drones/control/MPCControl.py
+++ gym_pybullet_drones/control/MPCControl.py
@@ -6,10 +6,7 @@ import pkg_resources
 from gym_pybullet_drones.utils.enums import DroneModel
 
 class MPCControl(BaseControl):
-    """Base class for control.
-
-    Implements `__init__()`, `reset(), and interface `computeControlFromState()`,
-    the main method `computeControl()` should be implemented by its subclasses.
+    """MPC class for control.
 
     """
 
@@ -52,48 +49,6 @@ class MPCControl(BaseControl):
 
     ################################################################################
 
-    def computeControlFromState(self,
-                                control_timestep,
-                                state,
-                                target_pos,
-                                target_rpy=np.zeros(3),
-                                target_vel=np.zeros(3),
-                                target_rpy_rates=np.zeros(3)
-                                ):
-        """Interface method using `computeControl`.
-
-        It can be used to compute a control action directly from the value of key "state"
-        in the `obs` returned by a call to BaseAviary.step().
-
-        Parameters
-        ----------
-        control_timestep : float
-            The time step at which control is computed.
-        state : ndarray
-            (20,)-shaped array of floats containing the current state of the drone.
-        target_pos : ndarray
-            (3,1)-shaped array of floats containing the desired position.
-        target_rpy : ndarray, optional
-            (3,1)-shaped array of floats containing the desired orientation as roll, pitch, yaw.
-        target_vel : ndarray, optional
-            (3,1)-shaped array of floats containing the desired velocity.
-        target_rpy_rates : ndarray, optional
-            (3,1)-shaped array of floats containing the desired roll, pitch, and yaw rates.
-
-        """
-        return self.computeControl(control_timestep=control_timestep,
-                                   cur_pos=state[0:3],
-                                   cur_quat=state[3:7],
-                                   cur_vel=state[10:13],
-                                   cur_ang_vel=state[13:16],
-                                   target_pos=target_pos,
-                                   target_rpy=target_rpy,
-                                   target_vel=target_vel,
-                                   target_rpy_rates=target_rpy_rates
-                                   )
-
-    ################################################################################
-
     def computeControl(self,
                        control_timestep,
                        cur_pos,
@@ -130,87 +85,112 @@ class MPCControl(BaseControl):
         target_rpy_rates : ndarray, optional
             (3,1)-shaped array of floats containing the desired roll, pitch, and yaw rates.
 
+        Returns
+        -------
+        ndarray
+            (4,1)-shaped array of integers containing the RPMs to apply to each of the 4 motors.
+        ndarray
+            (3,1)-shaped array of floats containing the current XYZ position error.
+        float
+            The current yaw error.
+
         """
-        raise NotImplementedError
+        rpm = [0, 0, 0, 0]
+        pos_e = [0, 0, 0]
+        self.control_counter += 1
 
-################################################################################
+        thrust, computed_target_rpy, pos_e = self._MPCPositionControl(control_timestep,
+                                                                         cur_pos,
+                                                                         cur_quat,
+                                                                         cur_vel,
+                                                                         target_pos,
+                                                                         target_rpy,
+                                                                         target_vel
+                                                                         )
+        rpm = self._MPCAttitudeControl(control_timestep,
+                                          thrust,
+                                          cur_quat,
+                                          computed_target_rpy,
+                                          target_rpy_rates
+                                          )
 
-    def setPIDCoefficients(self,
-                           p_coeff_pos=None,
-                           i_coeff_pos=None,
-                           d_coeff_pos=None,
-                           p_coeff_att=None,
-                           i_coeff_att=None,
-                           d_coeff_att=None
-                           ):
-        """Sets the coefficients of a PID controller.
+        cur_rpy = p.getEulerFromQuaternion(cur_quat)
+        return rpm, pos_e, computed_target_rpy[2] - cur_rpy[2]
 
-        This method throws an error message and exist is the coefficients
-        were not initialized (e.g. when the controller is not a PID one).
+        ################################################################################
+
+        def _dslPIDPositionControl(self,
+                                control_timestep,
+                                cur_pos,
+                                cur_quat,
+                                cur_vel,
+                                target_pos,
+                                target_rpy,
+                                target_vel
+                                ):
+        """MPC position control.
 
         Parameters
         ----------
-        p_coeff_pos : ndarray, optional
-            (3,1)-shaped array of floats containing the position control proportional coefficients.
-        i_coeff_pos : ndarray, optional
-            (3,1)-shaped array of floats containing the position control integral coefficients.
-        d_coeff_pos : ndarray, optional
-            (3,1)-shaped array of floats containing the position control derivative coefficients.
-        p_coeff_att : ndarray, optional
-            (3,1)-shaped array of floats containing the attitude control proportional coefficients.
-        i_coeff_att : ndarray, optional
-            (3,1)-shaped array of floats containing the attitude control integral coefficients.
-        d_coeff_att : ndarray, optional
-            (3,1)-shaped array of floats containing the attitude control derivative coefficients.
-
-        """
-        ATTR_LIST = ['P_COEFF_FOR', 'I_COEFF_FOR', 'D_COEFF_FOR', 'P_COEFF_TOR', 'I_COEFF_TOR', 'D_COEFF_TOR']
-        if not all(hasattr(self, attr) for attr in ATTR_LIST):
-            print("[ERROR] in BaseControl.setPIDCoefficients(), not all PID coefficients exist as attributes in the instantiated control class.")
-            exit()
-        else:
-            self.P_COEFF_FOR = self.P_COEFF_FOR if p_coeff_pos is None else p_coeff_pos
-            self.I_COEFF_FOR = self.I_COEFF_FOR if i_coeff_pos is None else i_coeff_pos
-            self.D_COEFF_FOR = self.D_COEFF_FOR if d_coeff_pos is None else d_coeff_pos
-            self.P_COEFF_TOR = self.P_COEFF_TOR if p_coeff_att is None else p_coeff_att
-            self.I_COEFF_TOR = self.I_COEFF_TOR if i_coeff_att is None else i_coeff_att
-            self.D_COEFF_TOR = self.D_COEFF_TOR if d_coeff_att is None else d_coeff_att
-
-    ################################################################################
-    
-    def _getURDFParameter(self,
-                          parameter_name: str
-                          ):
-        """Reads a parameter from a drone's URDF file.
-
-        This method is nothing more than a custom XML parser for the .urdf
-        files in folder `assets/`.
-
-        Parameters
-        ----------
-        parameter_name : str
-            The name of the parameter to read.
+        control_timestep : float
+            The time step at which control is computed.
+        cur_pos : ndarray
+            (3,1)-shaped array of floats containing the current position.
+        cur_quat : ndarray
+            (4,1)-shaped array of floats containing the current orientation as a quaternion.
+        cur_vel : ndarray
+            (3,1)-shaped array of floats containing the current velocity.
+        target_pos : ndarray
+            (3,1)-shaped array of floats containing the desired position.
+        target_rpy : ndarray
+            (3,1)-shaped array of floats containing the desired orientation as roll, pitch, yaw.
+        target_vel : ndarray
+            (3,1)-shaped array of floats containing the desired velocity.
 
         Returns
         -------
         float
-            The value of the parameter.
+            The target thrust along the drone z-axis.
+        ndarray
+            (3,1)-shaped array of floats containing the target roll, pitch, and yaw.
+        float
+            The current position error.
 
         """
-        #### Get the XML tree of the drone model to control ########
-        URDF = self.DRONE_MODEL.value + ".urdf"
-        path = pkg_resources.resource_filename('gym_pybullet_drones', 'assets/'+URDF)
-        URDF_TREE = etxml.parse(path).getroot()
-        #### Find and return the desired parameter #################
-        if parameter_name == 'm':
-            return float(URDF_TREE[1][0][1].attrib['value'])
-        elif parameter_name in ['ixx', 'iyy', 'izz']:
-            return float(URDF_TREE[1][0][2].attrib[parameter_name])
-        elif parameter_name in ['arm', 'thrust2weight', 'kf', 'km', 'max_speed_kmh', 'gnd_eff_coeff' 'prop_radius', \
-                                'drag_coeff_xy', 'drag_coeff_z', 'dw_coeff_1', 'dw_coeff_2', 'dw_coeff_3']:
-            return float(URDF_TREE[0].attrib[parameter_name])
-        elif parameter_name in ['length', 'radius']:
-            return float(URDF_TREE[1][2][1][0].attrib[parameter_name])
-        elif parameter_name == 'collision_z_offset':
-            COLLISION_SHAPE_OFFSETS = [float(s) for s in URDF_TREE[1][2][0].attrib['xyz'].split(' ')]
-            return COLLISION_SHAPE_OFFSETS[2]
+
+        raise NotImplementedError
+
+        ################################################################################
+
+        def _MPCAttitudeControl(self,
+                               control_timestep,
+                               thrust,
+                               cur_quat,
+                               target_euler,
+                               target_rpy_rates
+                               ):
+        """MPC attitude control.
+
+        Parameters
+        ----------
+        control_timestep : float
+            The time step at which control is computed.
+        thrust : float
+            The target thrust along the drone z-axis.
+        cur_quat : ndarray
+            (4,1)-shaped array of floats containing the current orientation as a quaternion.
+        target_euler : ndarray
+            (3,1)-shaped array of floats containing the computed target Euler angles.
+        target_rpy_rates : ndarray
+            (3,1)-shaped array of floats containing the desired roll, pitch, and yaw rates.
+
+        Returns
+        -------
+        ndarray
+            (4,1)-shaped array of integers containing the RPMs to apply to each of the 4 motors.
+
+        """
+
+        raise NotImplementedError
+
+        
